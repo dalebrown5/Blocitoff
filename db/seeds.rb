1eff6662e909451a77ee04e0ec3da41e4a51d47f
Dale <dalebrown5@gmail.com>
2015-02-05 14:37:15 -0700

    Added Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'faker'
 
-# Create an admin user
+# Create test admin user
 admin = User.new(
   name:     'Admin User',
   email:    'admin@example.com',
@@ -9,21 +9,49 @@ admin = User.new(
 admin.skip_confirmation!
 admin.save!
 
-# Create List
-1.times do
-  List.create(
-    user: admin,
-    title: 'Admin List'
+# Create test member user
+member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
+member.skip_confirmation!
+member.save!
+
+
+# Create users
+3.times do
+  user = User.new(
+  name:     Faker::Name.name,
+  email:    Faker::Internet.email,
+  password: Faker::Lorem.characters(10)
   )
+  user.skip_confirmation!
+  user.save!
 end
 
-# # Create Items
-# 15.times do
-#   Item.create(
-#     list: 'Admin List',
-#     name: Faker::Company.bs
-#   )
-# end
+users = User.all
+
+# Create List
+users.each do |user|
+  1.times do 
+    List.create!(
+      user: admin,
+      title: Faker::Lorem.sentence(3)
+  )
+  end
+end
+lists = List.all
+
+# Create Items
+lists.each do |list|
+  10.times do
+    Item.create!(
+      list: list,
+      name: Faker::Company.bs
+  )
+  end
+end
 
 puts "Seed finished"
 puts "#{User.count} users created"
