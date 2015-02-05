ee17ac4b7b1ae30a216579ba98541e22c09880c9
Dale <dalebrown5@gmail.com>
2015-02-05 15:16:33 -0700

    Fix on seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ member = User.new(
   password: 'helloworld'
 )
 member.skip_confirmation!
-member.save!
+member.save
 
 
 # Create users
@@ -27,7 +27,7 @@ member.save!
   password: Faker::Lorem.characters(10)
   )
   user.skip_confirmation!
-  user.save!
+  user.save
 end
 
 users = User.all
@@ -36,8 +36,8 @@ users = User.all
 users.each do |user|
   1.times do 
     List.create!(
-      user: admin,
-      title: Faker::Lorem.sentence(3)
+    user: user,
+    title: Faker::Lorem.sentence(3)
   )
   end
 end
@@ -47,8 +47,8 @@ lists = List.all
 lists.each do |list|
   10.times do
     Item.create!(
-      list: list,
-      name: Faker::Company.bs
+    list: list,
+    name: Faker::Company.bs
   )
   end
 end
