8b59285305baf3376073707dec181aa8197f7c8c
Dale <dalebrown5@gmail.com>
2015-02-07 13:32:35 -0700

    Time left formula correction

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,8 @@
 class Item < ActiveRecord::Base
   belongs_to :list
 
-   def days_left
-    (DateTime.now.to_date - (item.created_at.to_date + 7)).to_i  
-   end
+  def days_left
+    ((created_at.to_date + 7) - DateTime.now.to_date).to_i 
+  end
 
 end
-<%= item.days_left %>
